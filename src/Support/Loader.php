f28e9080c89730e9a2278d18e486f609c2ea453e
Minh Pham <smpleader@gmail.com>
2022-01-31 20:17:05 +0700

    Add Support/Loader

---
+++ src/Support/Loader.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * SPT software - Loader
+ * 
+ * @project: https://github.com/smpleader/spt
+ * @author: Pham Minh - smpleader
+ * @description: support to load a class
+ * 
+ */
+
+namespace SPT\Support;
+
+class Loader
+{
+    public static function findClass($dir, $namespace='')
+    {
+        $tmp = [];
+        $objects = scandir($dir);
+        foreach ($objects as $x) 
+        { 
+            if ($x != '.' && $x != '..')
+            {
+                if( is_dir($dir. '/'. $x) )
+                {
+                    $tmp = array_merge( $tmp, $this->findClass($dir. '/'. $x, $x));
+                }
+                elseif(!is_link($dir. '/'. $x) && '.php' == substr($x, -4))
+                {
+                    $x = substr($x, 0, (strlen($x) - 4));
+                    $tmp[] = empty( $namespace ) ? $x : $namespace. '\\'.$x;
+                }
+            }
+        }
+        return $tmp;
+    }
+}

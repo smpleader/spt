1524bcc6ff922407628c6a41ee9b76ce84bf308d
joomaio.tinhcv <cvtinh2000@gmail.com>
2022-02-17 16:13:51 +0700

    fix name class Trait into Triat

--- src/View/Gui/Form.php
+++ src/View/Gui/Form.php
@@ -12,7 +12,7 @@ namespace SPT\View\Gui;
 
 class Form
 {
-    use SPT\Trait\Index;
+    use SPT\Triat\Index;
 
     protected $record; 
     protected $fields;
--- src/View/Gui/Listing.php
+++ src/View/Gui/Listing.php
@@ -12,7 +12,7 @@ namespace SPT\View\Gui;
 
 class Listing
 { 
-    use SPT\Trait\Index;
+    use SPT\Triat\Index;
     
     protected $items;
     protected $total;

45805d351a33c999bb3c21f4f3ac054946634edc
Minh Pham <smpleader@gmail.com>
2022-02-22 16:15:17 +0700

    fix logic bug in format

--- src/View/VM/ViewModel.php
+++ src/View/VM/ViewModel.php
@@ -91,7 +91,7 @@ class ViewModel implements ViewModelAdapter
         $this->view->set($key, $value, $shareable);
     }
 
-    public function state($key, $default='', $format='', $request='post', $sessionName='')
+    public function state($key, $default='', $format='cmd', $request='post', $sessionName='')
     {
         if(empty($sessionName)) $sessionName = $key;
 
@@ -103,11 +103,7 @@ class ViewModel implements ViewModelAdapter
         }
         else
         {
-            $var = $this->request->{$request}->get($key, $old);
-            if($format)
-            {
-                $var = Filter::{$format}($var);
-            }
+            $var = $this->request->{$request}->get($key, $old, $format);
             $this->session->set($sessionName, $var);
         }
 

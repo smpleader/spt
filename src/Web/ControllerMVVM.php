12b7ee59f787b0002aaa64b6abb19f10ac712d9f
Minh Pham <smpleader@gmail.com>
2023-06-22 15:31:07 +0700

    only VM within the plugin

--- src/Web/ControllerMVVM.php
+++ src/Web/ControllerMVVM.php
@@ -31,36 +31,38 @@ class ControllerMVVM extends Controller
 
     public function registerViewModels()
     {
+        $plgName = $this->app->get('currentPlugin', '');
+        $vmFolder = SPT_PLUGIN_PATH. '/plugins/'. $plgName. '/viewmodels';
+                
+        if( is_dir( $vmFolder))
+        {
+            foreach(new \DirectoryIterator($vmFolder) as $file) 
+            {
+                if (!$file->isDot() && $file->isFile()) 
+                {
+                    $filename = $file->getBasename(); 
+                    $vmName = substr($filename, 0, strlen($filename) - 4) ;
+                    $vmName = ucfirst($vmName);
+                    $vmName = $this->app->getNamespace(). '\\plugins\\'. $plgName. '\\viewmodels\\'. $vmName;
+
+                    if(class_exists($vmName))
+                    {
+                        ViewModelHelper::prepareVM(
+                            $vmName, 
+                            $vmName::register(), 
+                            $this->getContainer()
+                        );
+                    }
+                }
+            }
+        }
+        /* STOP to load VM from all plugins
         foreach(new \DirectoryIterator(SPT_PLUGIN_PATH) as $plg) 
         {
             if (!$plg->isDot() && $plg->isDir()) 
             { 
                 $plgName = $plg->getBasename();
-                $vmFolder = $plg->getPath(). '/'. $plgName. '/viewmodels';
-                
-                if( is_dir( $vmFolder))
-                {
-                    foreach(new \DirectoryIterator($vmFolder) as $file) 
-                    {
-                        if (!$file->isDot() && $file->isFile()) 
-                        {
-                            $filename = $file->getBasename(); 
-                            $vmName = substr($filename, 0, strlen($filename) - 4) ;
-                            $vmName = ucfirst($vmName);
-                            $vmName = $this->app->getNamespace(). '\\plugins\\'. $plgName. '\\viewmodels\\'. $vmName;
-
-                            if(class_exists($vmName))
-                            {
-                                ViewModelHelper::prepareVM(
-                                    $vmName, 
-                                    $vmName::register(), 
-                                    $this->getContainer()
-                                );
-                            }
-                        }
-                    }
-                }
             }
-        }
+        }*/
     }
 }

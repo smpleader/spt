b75656012bddd3ced042d4312deb14ae2a92b33c
Minh Pham <smpleader@gmail.com>
2023-11-07 14:57:46 +0700

    Update StaticObj.php
    update comments

--- src/StaticObj.php
+++ src/StaticObj.php
@@ -4,7 +4,7 @@
  * 
  * @project: https://github.com/smpleader/spt
  * @author: Pham Minh - smpleader
- * @description: Just a static singleton object
+ * @description: A static object support dynamic properties
  * 
  */
 
@@ -14,6 +14,15 @@ use SPT\Support\FncArray as Arr;
 
 class StaticObj 
 {
+    /**
+     * Assign value into internal variable array by key
+     * Accept overwrite if existing or not
+     *
+     * @param string   $key  internal variable array key name
+     * @param mixed    $value Assign value
+     * 
+     * @return void
+     */ 
     static function set($key, $value, $overwrite = true)
     {
         if( $overwrite || !isset( static::$_vars[$key] ) )
@@ -21,17 +30,37 @@ class StaticObj
             static::$_vars[$key] = $value;
         }
     }
-
+    
+    /**
+     * Get value from internal variable array by key
+     *
+     * @param string   $key  internal variable array key name
+     * @param mixed    $default Return value $default if key not found
+     * 
+     * @return mixed    
+     */ 
     static function get($key, $default = null)
     {
         return isset(static::$_vars[$key]) ? static::$_vars[$key] : $default;
     }
-
+    
+    /**
+     * Get internal variable array
+     * 
+     * @return array    In theory, it can be various depend on variable declaration    
+     */ 
     static function getAll()
     {
         return static::$_vars;
     }
-
+    
+    /**
+     * Fast way to setup internal variable array
+     *
+     * @param array   $arr  array to be merged into internal variable
+     * 
+     * @return void    
+     */ 
     static function importArr(array $arr)
     {
         Arr::merge(static::$_vars, $arr);

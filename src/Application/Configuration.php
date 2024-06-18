452bf1298a176354eb6d190ca49b3977e9d138fc
Pham Minh <smpleader@gmail.com>
2024-06-18 15:14:12 +0700

    create inner loop for the configuration

--- src/Application/Configuration.php
+++ src/Application/Configuration.php
@@ -15,56 +15,56 @@ use SPT\MagicObj;
 
 class Configuration extends MagicObj
 {
+    /**
+     * Internal array for environments
+     * @var array $_envs
+     */
+    protected $_envs = [];
+
     public function __construct($default)
     {
         defined('SPT_CONFIG_PATH') or die('Configuration path not found');
 
         $this->_vars = [];
         $this->_default = $default;
+        $this->import(SPT_CONFIG_PATH, $this);
+    }
 
-        // TODO: consider file name / folder name as a scope of information
-        if(is_file(SPT_CONFIG_PATH))
+    private function import(string $path, &$_var)
+    {
+        if(is_file($path))
         {
-            $this->import(SPT_CONFIG_PATH, $this);
+            $try = require_once $path;
+            if(is_array($try))
+            {
+                foreach ($try as $key => $value) {
+                    if(!is_numeric($key))
+                    {
+                        $_var->{$key} = $value;
+                    }
+                } 
+            }
         }
-        elseif(is_dir(SPT_CONFIG_PATH))
+        elseif(is_dir($path))
         {
-            foreach(new \DirectoryIterator(SPT_CONFIG_PATH) as $item) 
+            foreach(new \DirectoryIterator($path) as $item) 
             {
                 if (!$item->isDot())
-                { 
+                {
                     if($item->isFile() && 'php' == $item->getExtension())
                     {
-                        $this->import( SPT_CONFIG_PATH. '/'. $item->getBasename(), $this);
+                        $name =  $item->getBasename('.php');
+                        $_var->{$name} = new MagicObj($this->_default);
+                        $this->import( $path. '/'. $item->getBasename(), $_var->{$name});
                     }
                     elseif($item->isDir())
                     {
                         $name =  $item->getBasename();
-                        $this->{$name} = new MagicObj($default);
-                        foreach(new \DirectoryIterator(SPT_CONFIG_PATH. '/'. $name) as $inner) 
-                        {
-                            if (!$inner->isDot() && $inner->isFile() && 'php' == $inner->getExtension())
-                            {
-                                $this->import(SPT_CONFIG_PATH. '/'. $name. '/'. $inner->getBasename(), $this->{$name});
-                            }
-                        }
+                        $_var->{$name} = new MagicObj($this->_default);
+                        $this->import( $path. '/'. $item->getBasename(), $_var->{$name});
                     }
                 }
             }
-        }
-    }
-
-    private function import(string $path, &$b)
-    {
-        $try = require_once $path;
-        if(is_array($try) || is_object($try))
-        {
-            foreach ($try as $key => $value) {
-                if(!is_numeric($key))
-                {
-                    $b->{$key} = $value;
-                }
-            } 
         }
     }
 
@@ -76,5 +76,19 @@ class Configuration extends MagicObj
     public function empty($key)
     {
         return empty($this->_vars[$key]);
+    }
+
+    public function setEnv(string $path)
+    {
+        $configs = parse_ini_file($path); 
+        if( count($configs) && is_array($configs))
+        {
+            $this->_envs = $configs;
+        }
+    }
+
+    public function env($key)
+    {
+        return isset($this->_envs[$key]) ? $this->_envs[$key] : $this->_default;
     }
 }

18236287d0c6796afba8b8b40bceea67fc0fea0d
Pham Minh <smpleader@gmail.com>
2024-06-18 15:15:51 +0700

    support get config in env file

--- src/Application/Configuration.php
+++ src/Application/Configuration.php
@@ -15,6 +15,12 @@ use SPT\MagicObj;
 
 class Configuration extends MagicObj
 {
+    /**
+     * Internal array for environments
+     * @var array $_envs
+     */
+    protected $_envs = [];
+
     public function __construct($default)
     {
         defined('SPT_CONFIG_PATH') or die('Configuration path not found');
@@ -76,5 +82,19 @@ class Configuration extends MagicObj
     public function empty($key)
     {
         return empty($this->_vars[$key]);
+    }
+    
+    public function setEnv(string $path)
+    {
+        $configs = parse_ini_file($path); 
+        if( count($configs) && is_array($configs))
+        {
+            $this->_envs = $configs;
+        }
+    }
+
+    public function env($key)
+    {
+        return isset($this->_envs[$key]) ? $this->_envs[$key] : $this->_default;
     }
 }

618536c7ce703fda60aee0f2ba9f41f1725d5269
Pham Minh <smpleader@gmail.com>
2025-01-03 18:41:06 +0700

    stop magic methods with Config

---
+++ src/DynamicObj.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * SPT software - Dynamic Object
+ * 
+ * @project: https://github.com/smpleader/spt
+ * @author: Pham Minh - smpleader
+ * @description: A basic object support dynamic properties
+ * 
+ */
+
+namespace SPT;
+
+#[\AllowDynamicProperties]
+class DynamicObj {}

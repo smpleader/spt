b92da3b0a3b79e4521ed3c32753e910d406547ba
Pham Minh <smpleader@gmail.com>
2025-05-27 18:16:39 -0700

    support get data from env file

---
+++ src/Env.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * SPT software - Log class
+ * 
+ * @project: https://github.com/smpleader/spt
+ * @author: Pham Minh - smpleader
+ * @description: Calss support to log information for a debug
+ * 
+ */
+
+namespace SPT;
+
+class Env
+{
+    /**
+     * Internal array
+     * @var array $_vars
+     */
+    static protected $_vars = array();
+
+    /**
+     * Get value from internal variable array by key
+     *
+     * @param string   $key  internal variable array key name
+     * @param mixed    $default Return value $default if key not found
+     * 
+     * @return mixed    
+     */ 
+    static function get($key, $default = null)
+    {
+        return isset(static::$_vars[$key]) ? static::$_vars[$key] : $default;
+    }
+
+    /**
+     * Pass any parameters into internal _vars
+     *
+     * @param string   $path path to env file
+     * 
+     * @return void load data from env file once
+     */ 
+    public static function load( string $path)
+    {
+        if( file_exists($path. '/.env') && !count(static::$_vars))
+        {
+            static::$_vars = parse_ini_file($path. '/.env');
+        }
+    }
+}

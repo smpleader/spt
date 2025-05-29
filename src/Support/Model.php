d35c0e5a671b805fad8b88772c90adf673bfdca8
Pham Minh <smpleader@gmail.com>
2025-05-28 20:29:18 -0700

    do not force Model as SPT\Web\Model

--- src/Support/Model.php
+++ src/Support/Model.php
@@ -12,21 +12,20 @@ namespace SPT\Support;
 
 use SPT\Support\App;
 use SPT\Support\Loader;
-use SPT\Web\Model as WebModel;
 
 class Model
 {
     public static function containerize(string $classname, string $fullname, ?string $alias)
     {
-        if($fullname instanceof WebModel)
-        {
-            $container = App::getInstance()->getContainer();
-            $container->containerize(
-                $classname. 'Model', 
-                $fullname,
-                new $fullname($container),
-                $alias
-            );
-        }
+        // currently do not force Model as SPT\Web\Model
+        //$reflected = new ReflectionClass( $fullname );
+        //$reflected->isSubclassOf( '\SPT\Web\Model' )
+        $container = App::getInstance()->getContainer();
+        $container->containerize(
+            $classname. 'Model', 
+            $fullname,
+            new $fullname($container),
+            $alias
+        );
     }
 }
